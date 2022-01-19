a9e8e61bb466155a5a73452fbab22956d45d2ecb
s3813866 <tehyeuhaw@gmail.com>
2022-01-19 21:41:18 +0800

    Transaction model added

---
+++ Assignment2/Models/Transaction.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assignment2.Models;
+
+public enum TransactionType
+{
+    Deposit = 1,
+    Withdraw = 2,
+    Transfer = 3,
+    ServiceCharge = 4
+}
+
+public class Transaction
+{
+    [Required]
+    public int TransactionID { get; set; }
+
+    [Required]
+    public TransactionType TransactionType { get; set; }
+
+    [Required, ForeignKey("Account")]
+    public int AccountNumber { get; set; }
+    public virtual Account Account { get; set; }
+
+    [ForeignKey("DestinationAccount")]
+    public int? DestinationAccountNumber { get; set; }
+    public virtual Account DestinationAccount { get; set; }
+
+    [Required, Column(TypeName = "money")]
+    public decimal Amount { get; set; }
+
+    [StringLength(30)]
+    public string Comment { get; set; }
+
+    [Required]
+    public DateTime TransactionTimeUtc { get; set; }
+}
