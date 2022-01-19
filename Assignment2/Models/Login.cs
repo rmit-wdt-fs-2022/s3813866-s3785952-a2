15dae8893f0cd6a28ab59f1548ea1c6d9311358b
s3813866 <tehyeuhaw@gmail.com>
2022-01-19 21:25:33 +0800

    Login Model added

---
+++ Assignment2/Models/Login.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assignment2.Models;
+
+public class Login
+{
+    [Column(TypeName = "char")]
+    [StringLength(8)]
+    public string LoginID { get; set; }
+
+    [Required]
+    [ForeignKey("Customer")]
+    public int CustomerID { get; set; }
+    
+    public virtual Customer Customer { get; set; }
+
+    [Column(TypeName = "char")]
+    [Required, StringLength(64)]
+    public string PasswordHash { get; set; }
+}
