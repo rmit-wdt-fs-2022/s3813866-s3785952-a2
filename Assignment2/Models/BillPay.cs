94efe59382c8cd7949a65a31f4c8beee095a5378
s3813866 <tehyeuhaw@gmail.com>
2022-01-19 21:48:59 +0800

    BillPay model added

---
+++ Assignment2/Models/BillPay.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assignment2.Models;
+
+public enum Period
+{
+    OneOff = 'O',
+    Monthly = 'M'
+}
+
+public class BillPay
+{
+    [Required]
+    public int BillPayID { get; set; }
+    
+    [Required, ForeignKey("Account")]
+    public int AccountNumber { get; set; }
+    
+    [Required, ForeignKey("Payee")]
+    public int PayeeID { get; set; }
+    
+    [Required]
+    public decimal Amount {get; set;}
+    
+    [Required]
+    public DateTime ScheduleTimeUtc {get; set;}
+    
+    [Required]
+    public Period Period {get; set;}
+}
