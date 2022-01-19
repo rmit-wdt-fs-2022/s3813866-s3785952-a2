2c8114e14d5ea8e52a3a4e25e2870a9a0a01bbe8
s3813866 <tehyeuhaw@gmail.com>
2022-01-19 20:56:19 +0800

    Account Model added

---
+++ Assignment2/Models/Account.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assignment2.Models;
+
+public enum AccountType
+{
+    Checking = 1,
+    Saving = 2
+}
+
+public class Account
+{
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+    
+    [Required, Display(Name = "Account Number")]
+    public int AccountNumber { get; set; }
+
+    [Required ,Display(Name = "Type")]
+    public AccountType AccountType { get; set; }
+
+    [Required]
+    public int CustomerID { get; set; }
+    
+    public virtual Customer Customer { get; set; }
+
+    [Column(TypeName = "money")]
+    [DataType(DataType.Currency)]
+    public decimal Balance { get; set; }
+    
+    //[InverseProperty("Account")]
+    // public virtual List<Transaction> Transactions { get; set; }
+}
