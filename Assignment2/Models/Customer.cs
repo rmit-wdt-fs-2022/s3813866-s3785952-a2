a8d9ecc8d489f80d77ee2f6a2931243db6532582
s3813866 <tehyeuhaw@gmail.com>
2022-01-19 20:50:21 +0800

    Customer model added

---
+++ Assignment2/Models/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace McbaExample.Models;
+
+public class Customer
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    public int CustomerID { get; set; }
+
+    [Required, StringLength(50)]
+    
+    public string Name { get; set; }
+    
+    [StringLength(11)]
+    public string TFN { get; set; }
+
+    [StringLength(50)]
+    public string Address { get; set; }
+
+    [StringLength(40)]
+    public string Suburb { get; set; }
+    
+    [StringLength(3)]
+    public string Postcode { get; set; }
+
+    [StringLength(4)]
+    public string PostCode { get; set; }
+    
+    [StringLength(12)]
+    public string Mobile { get; set; }
+
+    // public virtual List<Account> Accounts { get; set; }
+}
