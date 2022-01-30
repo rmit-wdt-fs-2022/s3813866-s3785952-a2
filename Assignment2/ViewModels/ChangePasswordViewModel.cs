f7b9cb8b8ad4d850af46641a4257a0095bbc4ee0
Andrew Tran <tandrew468@gmail.com>
2022-01-30 16:27:47 +1100

    Added ChangePasswordViewModel.cs

---
+++ Assignment2/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Assignment2.Models;
+
+namespace Assignment2.ViewModels;
+
+public class ChangePasswordViewModel
+{
+    public Login Login { get; set; }
+    
+    [Required]
+    [MinLength(8)]
+    [MaxLength(20)]
+    public string Password { get; set; }
+    
+    [Required]
+    [MinLength(8)]
+    [MaxLength(20)]
+    public string ConfirmPassword { get; set; }
+    
+}
