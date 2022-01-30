392b4de71018a6614e13e6d114e1b0644c3ffea0
Andrew Tran <tandrew468@gmail.com>
2022-01-30 20:16:53 +1100

    Refactored login model to display proper name in login page

--- Assignment2/Models/Login.cs
+++ Assignment2/Models/Login.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Assignment2.Models;
@@ -6,6 +7,7 @@ namespace Assignment2.Models;
 public class Login
 {
     [Column(TypeName = "char")]
+    [DisplayName("Login Identification")]
     [StringLength(8)]
     public string LoginId { get; set; }
 
