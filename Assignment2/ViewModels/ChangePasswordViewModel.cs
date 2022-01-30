2445d3b0ea2bfaaabdee79047b639e342b1eeddb
Andrew Tran <tandrew468@gmail.com>
2022-01-30 18:42:24 +1100

    Added More Model Validation for password view model

--- Assignment2/ViewModels/ChangePasswordViewModel.cs
+++ Assignment2/ViewModels/ChangePasswordViewModel.cs
@@ -1,20 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Assignment2.Models;
+using Newtonsoft.Json.Serialization;
 
 namespace Assignment2.ViewModels;
 
 public class ChangePasswordViewModel
 {
-    public Login Login { get; set; }
-    
     [Required]
-    [MinLength(8)]
-    [MaxLength(20)]
+    [MinLength(8, ErrorMessage = "Password must be a minimum of 8 characters")]
+    [MaxLength(20, ErrorMessage = "Password is too long maximum number of characters is 20")]
+    [DataType(DataType.Password)]
     public string Password { get; set; }
-    
+
     [Required]
-    [MinLength(8)]
-    [MaxLength(20)]
+    [MinLength(8, ErrorMessage = "Password must be a minimum of 8 characters")]
+    [MaxLength(20, ErrorMessage = "Password is too long maximum number of characters is 20")]
+    [CompareAttribute("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    [DataType(DataType.Password)]
     public string ConfirmPassword { get; set; }
-    
 }
